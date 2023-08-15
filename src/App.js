1b6c01382c8310fc56665a7fc7f73d7b2d1da2c8
Haroun Ted <harounted@gmail.com>
2023-08-15 16:04:54 +0300

    add pda images

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import ProfileCard from "./ProfileCard";
+import GoogleImage from './images/googleAssistant.png';
+import CortanaImage from './images/cortana.png';
+import SiriImage from './images/siri.png';
+
 
 function App(){
     return(
         <div>
-        <div>
-            Personal Degital Assistants
-        </div>
-        <ProfileCard title="Google Assistant" handle="@googleAssistant99"/>
-        <ProfileCard title="Cortana" handle="@cortana32"/>
-        <ProfileCard title="Siri" handle="@siri01"/>
+        <div>Personal Degital Assistants</div>
+        <ProfileCard title="Google Assistant" handle="@googleAssistant99" image = {{ src: GoogleImage, alt: "Google Assistant Logo" }}/>
+        <ProfileCard title="Cortana" handle="@cortana32" image = {{src: CortanaImage, alt: "Cortana Logo"}}/>
+        <ProfileCard title="Siri" handle="@siri01" image = {{src: SiriImage, alt: "Siri Logo"}}/>
     </div>
     )
    
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,6 +1,9 @@
-function ProfileCard({title, handle}){
+function ProfileCard({title, handle, image}){
+    // console.log({title, handle})
     return (
+
         <div>
+            <img src = {image.src} width={250} alt={image.alt}/>
             <div>Title is {title}</div>
             <div>Twitter handle is {handle}</div>
         </div>
